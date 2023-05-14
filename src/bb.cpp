6e6a6981151a6518e4b64164c2a417d9aef29008
MarioLndz <mariolindez@gmail.com>
2023-05-14 19:48:03 +0200

    BB Revisado

--- src/bb.cpp
+++ src/bb.cpp
@@ -1,265 +1,299 @@
 //
-// Created by mario on 6/05/23.
+// Created by mario on 13/05/23.
 //
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <algorithm>
-#include <chrono>
-#include <random>
-
+#include <set>
+#include <fstream>
+#include <queue>
 using namespace std;
 
-void ReduceFilas(vector<vector<int>> & reducida, vector<int> & minimo_fila, int num_nodos){
+pair<vector<int>, int> Greedy (const vector<vector<int>>& distancias, vector<bool> ya_pertenece, const int NUM_NODOS){
+    vector<int> visita;
 
-    int min=0;
+    visita.push_back(0);  // la empresa está en 0
+    int pos_actual = 0;   // posicion en la que estamos
 
+    int indice_minimo = 1;   // para calcular la siguiente posicion
+    int min_dist = distancias[pos_actual][indice_minimo];
 
-    // calculamos mínimo de cada fila
+    int distancia_solucion = 0;
 
-    for (int i = 0; i < num_nodos; ++i) {
-        min = 100;
-        for (int j = 0; j < num_nodos; ++j) {
-            if (reducida[i][j]!=(-1) && reducida[i][j] < min)
-                min = reducida[i][j];
-        }
-        if(min==100) {// en el caso de que todos sean -1
-            minimo_fila[i] = 0;
-        }
-        else{
-            minimo_fila[i]=min;  // vamos guardando el minimo de columna en el vector minimo
-        }
+    while (visita.size() < NUM_NODOS){
+        min_dist = 500;
+        for (int i = 1; i < NUM_NODOS; ++i){
+            int num = distancias[pos_actual][i];
 
-        minimo_fila[i] = min;  // vamos guardando el minimo de fila en el vector minimo
-    }
-
-    // una vez tenemos el mínimo de cada fila se lo restamos a dicha fila
-    for (int i = 0; i < num_nodos; ++i){
-        for (int j = 0; j < num_nodos; ++j){
-            if (i != j) {
-                reducida[i][j] -= minimo_fila[i];
+            if((!ya_pertenece[i]) && (num < min_dist) && (i != pos_actual)){
+                min_dist = num;
+                indice_minimo = i;
             }
         }
+        distancia_solucion += min_dist;
+
+        pos_actual = indice_minimo;
+        visita.push_back(pos_actual);
+        ya_pertenece[pos_actual] = true;
     }
+
+    distancia_solucion += distancias[visita.at(visita.size()-1)][visita.at(0)];
+
+    return (pair<vector<int>,int>(visita, distancia_solucion));
 
 }
 
-void ReduceColumnas(vector<vector<int>> & reducida, vector<int> & minimo_col, int num_nodos){
+struct nodo {
+    vector<int> solucion;
+    set<int> sin_visitar;
+    int coste;
 
-    int min=0;
+    nodo(){
+        coste = 0;
+    }
 
-    for (int i = 0; i < num_nodos; ++i) {
-        min=100;
-        for (int j = 0; j < num_nodos; ++j) {
-            if(reducida[j][i]!=(-1) && reducida[j][i]<min)
-                min=reducida[j][i];
-        }
-        if(min==100) {
-            minimo_col[i] = 0;
-        }
-        else{
-            minimo_col[i]=min;  // vamos guardando el minimo de columna en el vector minimo
+    nodo(const vector<int> & la_solucion, const set<int> & los_sin_visitar, int el_coste){
+        solucion = la_solucion;
+        sin_visitar = los_sin_visitar;
+        coste = el_coste;
+    }
+
+    int NodosVisitados () const{
+        return (solucion.size());
+    }
+
+    bool operator< (const nodo & otro) const {
+        return (this->coste > otro.coste);
+    }
+};
+
+
+struct Comparison {
+    bool operator() (const pair<nodo,int>& n1, const pair<nodo,int> & n2){
+        return (n1.second > n2.second);
+    }
+};
+
+class BB {
+private:
+    int NUM_NODOS;
+
+    vector<vector<int>> distancias;
+    int COTA_GLOBAL;
+
+    vector<int> salidas_minimas;
+
+    nodo origen;
+
+    vector<int> solucion;
+
+    priority_queue<pair<nodo,int>, vector<pair<nodo,int>>, Comparison> caminos;
+
+
+
+
+public:
+    BB (){
+        NUM_NODOS = 0;
+        COTA_GLOBAL = 0;
+    }
+
+    BB (const char filename []){
+        load(filename);
+    }
+
+    void load (const char filename[]){
+        ifstream file;
+        file.open(filename);
+
+        if (!file){
+            cout << "ERROR ABRIENDO EL ARCHIVO" << endl;
+            exit(-1);
         }
 
+        int nodos_aux;
+        file >> nodos_aux;
+
+        NUM_NODOS = nodos_aux;
+
+        reservaMemoria(NUM_NODOS);
+
+        set<int> set_origen;
+
+        for (int i = 0; i < NUM_NODOS; ++i) {
+            for (int j = 0; j < NUM_NODOS; ++j) {
+                int num;
+                file >> num;
+
+                distancias[i][j] = num;
+            }
+
+            if (i != 0){
+                set_origen.insert(i);
+            }
+
+        }
+
+        file.close();
+
+        origen = nodo(vector<int>{0}, set_origen, 0);
+
+        // Siempre partimos de la ciudad 0
+        //solucion.push_back(0);
+        //sin_visitar.erase(0);
+
+        vector<bool> ya_pertenece (NUM_NODOS);
+        ya_pertenece.at(0) = true;
+
+        CalculaSalidasMinimas();
+
+        pair<vector<int>, int> sol_greedy = Greedy (distancias, ya_pertenece, NUM_NODOS);
+        COTA_GLOBAL = sol_greedy.second;
+        solucion = sol_greedy.first;
 
     }
 
-    // restamos el valor minimo de cada columna a dicha columna
+    int GetCotaGlobal () const {
+        return (COTA_GLOBAL);
+    }
 
-    for (int i = 0; i < num_nodos; ++i) {
-        for (int j = 0; j < num_nodos; ++j) {
-            if (i!=j) {
-                reducida[j][i] -= minimo_col[i];
+    void PintaDistancias () const {
+        cout << "MATRIZ DE DISTANCIAS:\n";
+        for (int i = 0; i < NUM_NODOS; ++i){
+            for (int j = 0; j < NUM_NODOS; ++j){
+                cout << distancias[i][j] << "\t";
+            }
+            cout << endl;
+        }
+    }
+
+    void PintaSalidasMinimas () const {
+        cout << "SALIDAS MINIMAS:\t";
+        for (int i = 0; i < NUM_NODOS; ++i){
+            cout << salidas_minimas.at(i) << "\t";
+        }
+        cout << endl;
+    }
+
+    void PintaSolucion(int coste=-1) const {
+        if (coste == -1){
+            coste = CalculaCoste();
+        }
+
+        cout << "SOLUCION:\t[\t";
+        for (int i = 0; i < solucion.size(); ++i){
+            cout << solucion.at(i) << "\t";
+        }
+        cout << "]\tCoste: " << coste << endl;
+    }
+
+    void pvc2 () {
+        caminos.push(pair<nodo,int>{this->origen, CotaLocal2(this->origen)});
+
+        while (!caminos.empty()){
+            nodo prometedor = caminos.top().first;
+            caminos.pop();
+
+            if (prometedor.sin_visitar.empty()){
+                prometedor.coste += distancias[prometedor.solucion.back()][0];
+
+                if (prometedor.coste < COTA_GLOBAL){
+                    solucion = prometedor.solucion;
+                    COTA_GLOBAL = prometedor.coste;
+                }
+
+            } else {
+                for (auto it = prometedor.sin_visitar.begin(); it != prometedor.sin_visitar.end(); ++it){
+                    nodo aux = prometedor;
+
+                    aux.sin_visitar.erase(*it);
+                    aux.solucion.push_back(*it);
+                    aux.coste += distancias[prometedor.solucion.back()][*it];
+
+                    int cota = CotaLocal2(aux);
+
+                    if (cota < COTA_GLOBAL){
+                        caminos.push(pair<nodo,int>{aux,cota});
+                    }
+                }
+
+            }
+
+        }
+    }
+
+    int CotaLocal2 (const nodo & n){
+        int coste = n.coste;
+
+        coste += GetSalidaMinima(solucion.back());
+
+        for (auto it = n.sin_visitar.begin(); it != n.sin_visitar.end(); ++it){
+            coste += GetSalidaMinima(*it);
+        }
+
+        return (coste);
+    }
+
+    int GetSalidaMinima (int k){
+        return (distancias[k][salidas_minimas.at(k)]);
+    }
+
+private:
+    void reservaMemoria (const int & nodos){
+        distancias.resize(nodos);
+
+        for (int i = 0; i < nodos; ++i){
+            distancias[i].resize(nodos);
+        }
+    }
+
+    int SalidaMinima (const vector<int>& fila) const{
+        int indice_min = (fila[0] == -1) ? 1 : 0;
+
+        for (int i = indice_min+1; i < NUM_NODOS; ++i){
+            if (fila[i] != -1){   // Si no es la diagonal
+                if (fila[i] < fila[indice_min]){  // Nuevo minimo
+                    indice_min = i;
+                }
             }
         }
+
+        return (indice_min);
+
     }
 
-}
-
-void EliminaFilayColumna(vector<vector<int>> & reducida, int fila, int columna, int num_nodos){   // hace -1 todos los valores de la fila y la columna, ademas de la posición [col][fila]
-
-    for (int i = 0; i < num_nodos; ++i) {
-        reducida[fila][i]=-1;
+    void CalculaSalidasMinimas (){
+        for (int i = 0; i < NUM_NODOS; ++i){
+            salidas_minimas.push_back(SalidaMinima(distancias[i]));
+        }
     }
-    for (int i = 0; i < num_nodos; ++i) {
-        reducida[i][columna]=-1;
-    }
-    reducida[columna][fila]=-1;
-}
 
-int main (int argc, char * argv []) {
+    int CalculaCoste () const {
+        int coste = 0;
+        for (int i = 0; i < NUM_NODOS-1; ++i){
+            //cout << distancias[solucion.at(i)][solucion.at(i+1)] << " + ";
+            coste += distancias[solucion.at(i)][solucion.at(i+1)];
+        }
+        coste += distancias[solucion.back()][0];
+        //cout << distancias[solucion.back()][0] << " = " << coste << endl;
+
+        return (coste);
+    }
+
+};
+
+// https://code-with-me.global.jetbrains.com/u4l7RQH69xtukLfXXqQLkw#p=CL&fp=B7CF77FD7E61CC6ADE27EBA1EF1E04CF3DE6AD42766C5FE2F8A2222907289AE9
+
+int main (int argc, char * argv[]){
     if (argc != 2) {
         cout << "NUMERO INCORRECTO DE ARGUMENTOS" << endl;
         exit(-1);
     }
 
-    ifstream file;
-    file.open(argv[1]);
+    BB branch_bound (argv[1]);
 
-    if (!file) {
-        cout << "ERROR ABRIENDO EL ARCHIVO" << endl;
-        exit(-1);
-    }
+    //branch_bound.PintaDistancias();
+    //branch_bound.PintaSalidasMinimas();
 
-    int aux;
-    file >> aux;
-
-    const int NUM_NODOS = aux;
-
-    vector<vector<int>> distancias(NUM_NODOS);
-    for (int i = 0; i < NUM_NODOS; ++i) {
-        distancias[i].reserve(NUM_NODOS);
-    }
-
-    for (int i = 0; i < NUM_NODOS; ++i) {
-        for (int j = 0; j < NUM_NODOS; ++j) {
-            int num;
-            file >> num;
-            distancias[i][j] = num;
-        }
-    }
-
-    file.close();
-
-    /*
-    for (int i = 0; i < NUM_NODOS; ++i){
-        for (int j = 0; j < NUM_NODOS; ++j){
-            cout << distancias[i][j] << "\t";
-        }
-        cout << endl;
-    }
-     */
-
-    // reducimos la matriz de distancias
-
-    vector<vector<int>> reducida(NUM_NODOS);
-    for (int i = 0; i < NUM_NODOS; ++i) {
-        reducida[i].reserve(NUM_NODOS);
-    }
-
-    for (int i = 0; i < NUM_NODOS; ++i)
-        for (int j = 0; j < NUM_NODOS; ++j)
-            reducida[i][j]=distancias[i][j];
-
-
-    vector<int> minimo_fila(NUM_NODOS);
-
-    ReduceFilas(reducida,minimo_fila,NUM_NODOS);
-
-    cout << "FILAS MINIMAS:\t";
-    for(int i=0;i<NUM_NODOS;i++){
-        cout << minimo_fila[i] << "\t";
-    }
-    cout << endl;
-
-    for (int i = 0; i < NUM_NODOS; ++i){
-        for (int j = 0; j < NUM_NODOS; ++j){
-            cout << reducida[i][j] << "\t";
-        }
-        cout << endl;
-    }
-
-    // Tenemos que hacer lo mismo pero con las columnas
-
-    vector<int> minimo_col (NUM_NODOS);   // vamos a guadar el minimo de todas las columnas
-
-    ReduceColumnas(reducida,minimo_col,NUM_NODOS);
-
-    cout << "COLUMNAS MINIMAS:\t";
-    for(int i=0;i<NUM_NODOS;i++){
-        cout << minimo_col[i] << "\t";
-    }
-    cout << endl;
-
-    for (int i = 0; i < NUM_NODOS; ++i){
-        for (int j = 0; j < NUM_NODOS; ++j){
-            cout << reducida[i][j] << "\t";
-        }
-        cout << endl;
-    }
-
-    // calculamos el coste de la reducida
-
-    int coste_reducida=0;
-
-    for(int i=0;i<NUM_NODOS;++i){
-        coste_reducida+=minimo_fila[i];
-        coste_reducida+=minimo_col[i];
-    }
-
-
-    // ya tenemos nuestra matriz reducida y su coste, para probar que funciona:
-
-    for (int i = 0; i < NUM_NODOS; ++i){
-        for (int j = 0; j < NUM_NODOS; ++j){
-            cout << distancias[i][j] << "\t";
-        }
-        cout << endl;
-    }
-
-    cout << endl << endl << "COSTE:" << coste_reducida << endl << endl;
-
-
-    // tenemos que elegir cual será el siguiente nodo al que vayamos y para ello tendremos que ir
-    // considerando todos los nodos restantes uno a uno
-
-    vector<vector<vector<int>>> matrices_reducidas (NUM_NODOS);
-    for (int i = 0; i < NUM_NODOS; ++i) {
-        for(int j=0;j<NUM_NODOS;j++) {
-            matrices_reducidas[i][j].reserve(NUM_NODOS);
-        }
-    }
-
-    vector<int> costes(NUM_NODOS);
-
-    for(int i=1;i<NUM_NODOS;++i){   // buble para pasar por todos los nodos restantes, empezamos desde 1
-
-        // para acda nodo tendremos que poner a -1 la fila 0 y la columna i y
-        // posteirormente realizar la reducción por filas y por columnas
-
-        vector<vector<int>> reducida2(NUM_NODOS);
-        for (int z = 0; z < NUM_NODOS; ++z) {
-            reducida2[z].reserve(NUM_NODOS);  // aquí da problemas
-        }
-        cout << "p";
-        for (int z = 0; z < NUM_NODOS; ++z)
-            for (int j = 0; j < NUM_NODOS; ++j)
-                reducida2[z][j]=reducida[z][j];
-
-        EliminaFilayColumna(reducida2,0,i,NUM_NODOS);
-
-        vector<int> minimo_fila2 (NUM_NODOS);   // vamos a guadar el minimo de todas las filas
-        ReduceColumnas(reducida2,minimo_fila2,NUM_NODOS);
-
-        vector<int> minimo_col2 (NUM_NODOS);   // vamos a guadar el minimo de todas las columnas
-        ReduceColumnas(reducida2,minimo_col2,NUM_NODOS);
-
-        int coste_reducida2=0;
-        for(int z=0;z<NUM_NODOS;++z) {
-            coste_reducida2 += minimo_fila2[z];
-            coste_reducida2 += minimo_col2[z];
-        }
-        coste_reducida2 += coste_reducida;
-        coste_reducida2 += reducida[0][i];
-
-
-
-        for(int z=0;z<NUM_NODOS-1;++z){
-            for(int j=0;j<NUM_NODOS-1;++j){
-                matrices_reducidas[i-1][z][j]=reducida2[z][j];
-            }
-        }
-
-        costes[i-1]=coste_reducida2;
-
-    }
-
-    // una vez tenemos todas las matrices y todos los costes nos tenemos que quedar con la que tenga menor coste
-
-    for(int i=0;i<NUM_NODOS-1;++i){
-        cout << costes[i] << "\t";
-    }
-
-
-    return (0);
+    branch_bound.pvc2();
+    branch_bound.PintaSolucion();
 }
